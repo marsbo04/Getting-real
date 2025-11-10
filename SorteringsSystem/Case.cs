1e6048c424eda2f3f37f16d22fdfcc49bb8cb180
Nicklas Jensen <nicklasbjbj@gmail.com>
2025-11-10 14:01:05 +0100

    Lavet classes ud fra domænemodellen og givet dem deres attributter

---
+++ SorteringsSystem/Case.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SorteringsSystem
+{
+    public class Case
+    {
+        public enum Status
+        {
+            Rejected,      // Task was rejected
+            InProgress,    // Task is being filled out or started
+            Active,        // Task is currently being worked on
+            Completed      // Task is finished
+        }
+        public enum Prioritising
+        {
+            Low,
+            Medium, 
+            High
+        }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Deadline { get; set; }
+        public int TaskAmount { get; set; }
+        public string Note { get; set; }
+    }
+}
